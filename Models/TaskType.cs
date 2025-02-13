9404ac0647c57669591e4afea47c3ab82e8e2d7e
StolinskiJan <147153943+StolinskiJan@users.noreply.github.com>
2025-02-13 21:43:23 +0100

    Add files via upload

---
+++ Models/TaskType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PTr.Models
+{
+    public enum TaskTypeName
+    {
+        [Display(Name = "IT Specialist")]
+        ITSpecialist = 1,
+
+        [Display(Name = "Accountant")]
+        Accountant = 2,
+
+        [Display(Name = "Cleaner")]
+        Cleaner = 3
+    }
+    public class TaskType
+    {
+        public int Id { get; set; }
+        
+        public TaskTypeName Name { get; set; }
+        public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+
+    }
+}
